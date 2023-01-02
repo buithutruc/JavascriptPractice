9c20488fd3c80d279577eb6374dfc9be8c379314
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-01-01 20:36:51 -0500

    added decrement, save and result display functions

--- counter_app/counter.js
+++ counter_app/counter.js
@@ -1,7 +1,38 @@
 let countSum = document.getElementById("count_sum");
+let savedEntries = document.getElementById("save_el");
+let total = document.getElementById("total_el");
 let count = 0;
+let sum = 0;
 
 function increment() {
-  count = count + 1;
+  count += 1;
   countSum.innerText = count;
+}
+
+function decrement() {
+  if (count <= 0) {
+    count = 0;
+  } else {
+    count -= 1;
+    countSum.innerText = count;
+  }
+}
+
+function save() {
+  if (count != 0) {
+    let entry = count;
+    savedEntries.textContent += " " + entry + " - ";
+
+    //display total number of people
+    sum += entry;
+    calculateTotal();
+
+    //reset counting number
+    count = 0;
+    countSum.innerText = 0;
+  }
+}
+
+function calculateTotal() {
+  total.textContent = sum;
 }
