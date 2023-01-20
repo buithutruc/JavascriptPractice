ed5191a346a986908c3ccf474c73131f660b80d2
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-01-20 16:45:07 -0500

    finished practicing classes and json

---
+++ JsPractice/json_practice.js
@@ -0,0 +1,24 @@
+//JSON: JavaScript Object Notation
+
+const myObj = {
+  name: "Dave",
+  hobbies: ["eat", "sleep", "code"],
+  hello: function () {
+    console.log("Hello!");
+  },
+};
+
+console.log(myObj);
+console.log(myObj.name);
+myObj.hello();
+console.log(typeof myObj);
+
+const sendJSON = JSON.stringify(myObj); //lost the method
+console.log(sendJSON);
+
+console.log(typeof sendJSON);
+console.log(sendJSON.name);
+
+const receiveJSON = JSON.parse(sendJSON);
+console.log(receiveJSON);
+console.log(typeof receiveJSON);
