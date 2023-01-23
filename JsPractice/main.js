3140ea054f717c40704baf6498836cfae2db7ef5
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-01-22 20:03:16 -0500

    practicing DOM

--- JsPractice/main.js
+++ JsPractice/main.js
@@ -1,1 +1,18 @@
+//DOM - Document Object Model
+const view1 = document.getElementById("view1");
+console.log(view1);
 
+const view2 = document.querySelector("#view2");
+console.log(view2);
+
+view1.style.display = "none";
+view2.style.display = "flex";
+
+const views = document.getElementsByClassName("view");
+console.log(views);
+
+const sameViews = document.querySelectorAll(".view");
+console.log(sameViews);
+
+const divs = view1.querySelectorAll("div");
+console.log(divs);
