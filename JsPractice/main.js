77ee3d48ccb410b7b38dcab4d29511c14f14a497
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-01-31 19:35:38 -0500

    continued practicing modules

--- JsPractice/main.js
+++ JsPractice/main.js
@@ -5,10 +5,15 @@
 // import { shredding as shred, plucking as fingerpicking } from "./guitar.js";
 
 import * as Guitars from "./guitar.js";
+import User from "./user.js";
 
 // console.log(playGuitar());
 // console.log(shred());
 // console.log(fingerpicking());
+
+const me = new User("email@email.com", "Truc");
+console.log(me);
+console.log(me.greeting());
 
 console.log(Guitars.default());
 console.log(Guitars.shredding());
