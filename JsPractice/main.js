79fcd62285a6df9c7d4ce73faa56ede5d254a93e
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-02-01 23:39:30 -0500

    practiced higher order functions

--- JsPractice/main.js
+++ JsPractice/main.js
@@ -3,3 +3,31 @@
 //A higher order function is a function that does at least one of the following:
 //takes one or more functions as an argument (parameter)
 //returns a function as the result
+
+//forEach()
+
+import { posts } from "./posts.js";
+
+posts.forEach((post) => {
+  console.log(post);
+});
+
+console.clear();
+
+const filteredPosts = posts.filter((post) => {
+  return post.userId === 1;
+});
+
+console.log(filteredPosts);
+
+const mappedPosts = filteredPosts.map((post) => {
+  return post.id * 10;
+});
+
+console.log(mappedPosts);
+
+const reducedPostValue = mappedPosts.reduce((sum, post) => {
+  return sum + post;
+});
+
+console.log(reducedPostValue);
